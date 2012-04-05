1df71493ab08aa3ca552b7d34e5cde79aa76cbf4
Ryan Smith <rnsmith2@gmail.com>
2012-04-05 14:21:29 -0700

    Initialized Eclipse project for Graded Reader.

---
+++ gen/github/graded_reader/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package github.graded_reader;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/github/graded_reader/GradedReaderActivity.java
@@ -0,0 +1,13 @@
+package github.graded_reader;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GradedReaderActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
