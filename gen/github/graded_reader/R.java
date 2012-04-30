742de4cb0d096951e976e77b71d067bbe7e9cdfa
David J Cronkite <cronkd@uw.edu>
2012-04-30 08:55:30 -0700

    before merging

--- gen/github/graded_reader/R.java
+++ gen/github/graded_reader/R.java
@@ -14,23 +14,29 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int chapter=0x7f060003;
-        public static final int chapterText=0x7f060000;
-        public static final int grammarText=0x7f060001;
-        public static final int menu=0x7f060007;
-        public static final int sentenceText=0x7f060004;
-        public static final int settings=0x7f060008;
-        public static final int tabs=0x7f060002;
-        public static final int vocabularyText=0x7f060005;
-        public static final int wordText=0x7f060006;
+        public static final int chapter=0x7f060001;
+        public static final int chapterText=0x7f060002;
+        public static final int emptyView=0x7f060003;
+        public static final int grammarText=0x7f060006;
+        public static final int grammar_button=0x7f060004;
+        public static final int menu=0x7f06000a;
+        public static final int sentenceText=0x7f060007;
+        public static final int settings=0x7f06000b;
+        public static final int tab_fragment=0x7f060000;
+        public static final int vocab_button=0x7f060005;
+        public static final int vocabularyText=0x7f060008;
+        public static final int wordText=0x7f060009;
     }
     public static final class layout {
-        public static final int chapter=0x7f030000;
-        public static final int grammar=0x7f030001;
-        public static final int main=0x7f030002;
-        public static final int translate_sentence=0x7f030003;
-        public static final int vocabulary=0x7f030004;
-        public static final int word=0x7f030005;
+        public static final int activity_tab_fragment=0x7f030000;
+        public static final int chapter=0x7f030001;
+        public static final int empty=0x7f030002;
+        public static final int fragment_for_tabs=0x7f030003;
+        public static final int grammar=0x7f030004;
+        public static final int main=0x7f030005;
+        public static final int translate_sentence=0x7f030006;
+        public static final int vocabulary=0x7f030007;
+        public static final int word=0x7f030008;
     }
     public static final class menu {
         public static final int actionbar_menu=0x7f050000;
